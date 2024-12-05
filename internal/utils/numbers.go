78f8c9910d4d86d7ea552838d27ca33a2e55cd0d
spossner <github@possner.de>
2024-12-05 13:14:40 +0100

    must atoi

--- internal/utils/numbers.go
+++ internal/utils/numbers.go
@@ -1,8 +1,21 @@
 package utils
 
+import (
+	"log"
+	"strconv"
+)
+
 func AbsInt(n int) int {
 	if n < 0 {
 		return -n
+	}
+	return n
+}
+
+func MustAtoi(s string) int {
+	n, err := strconv.Atoi(s)
+	if err != nil {
+		log.Fatalf("cannot parse %s: %v\n", s, err)
 	}
 	return n
 }
