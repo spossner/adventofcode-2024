42d9bbd423de5c23db0d4e4fa1682951b6c5b1e5
spossner <github@possner.de>
2024-12-05 15:27:28 +0100

    support split by separator (empty one to separate each letter)

--- internal/config/config.go
+++ internal/config/config.go
@@ -3,13 +3,15 @@ package config
 const YEAR = 2024
 
 type Config struct {
-	Day        int
-	Dev        bool
-	DevFile    string
-	Strip      bool
-	SplitLines bool
-	SplitWords bool
-	ParseInts  bool
+	Day         int
+	Dev         bool
+	DevFile     string
+	Strip       bool
+	SplitLines  bool
+	SplitFields bool // splits each line by whitespaces (Fields
+	SplitWords  bool
+	SplitSep    string
+	ParseInts   bool
 }
 
 type ConfigFunc func(cfg *Config)
@@ -33,15 +35,24 @@ func WithDevFile(fileName string) ConfigFunc {
 	}
 }
 
+func WithSplitWords(sep string) ConfigFunc {
+	return func(cfg *Config) {
+		cfg.SplitWords = true
+		cfg.SplitSep = sep
+	}
+}
+
 func NewConfig(day int, dev bool, fn ...ConfigFunc) *Config {
 	cfg := &Config{
-		Day:        day,
-		Dev:        dev,
-		DevFile:    "dev.txt",
-		Strip:      true,
-		SplitLines: true,
-		SplitWords: false,
-		ParseInts:  false,
+		Day:         day,
+		Dev:         dev,
+		DevFile:     "dev.txt",
+		Strip:       true,
+		SplitLines:  true,
+		SplitFields: false,
+		SplitWords:  false,
+		SplitSep:    "",
+		ParseInts:   false,
 	}
 	for _, f := range fn {
 		f(cfg)
