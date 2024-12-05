09cc802d163e3d58675df63912f218c2b91e409e
spossner <github@possner.de>
2024-12-05 13:15:00 +0100

    third puzzle parsing text with regexp and doing multiplication

---
+++ 03/main.go
@@ -0,0 +1,56 @@
+package _0
+
+import (
+	"fmt"
+	"github.com/spossner/aoc2024/internal/config"
+	"github.com/spossner/aoc2024/internal/puzzle"
+	"github.com/spossner/aoc2024/internal/utils"
+	"regexp"
+)
+
+const DAY = 3
+
+func part1(dev bool) any {
+	return solve(dev, false)
+}
+
+func part2(dev bool) any {
+	return solve(dev, true)
+}
+
+func solve(dev bool, isPart2 bool) any {
+	if isPart2 {
+	}
+	cfg := config.NewConfig(DAY, dev, config.NoLineSplit)
+	if isPart2 {
+		cfg.DevFile = "dev-2.txt"
+	}
+	p := puzzle.NewPuzzle(cfg)
+	fmt.Println(p.Data)
+	result := 0
+
+	re := regexp.MustCompile(`mul\((\d{1,3}),(\d{1,3})\)`)
+	if isPart2 {
+		re = regexp.MustCompile(`mul\((\d{1,3}),(\d{1,3})\)|do\(\)|don't\(\)`)
+	}
+	matches := re.FindAllSubmatch([]byte(p.Data), -1)
+	active := true
+	for _, match := range matches {
+		if isPart2 {
+			if string(match[0]) == "do()" {
+				active = true
+				continue
+			}
+			if string(match[0]) == "don't()" {
+				active = false
+				continue
+			}
+		}
+
+		if !active {
+			continue
+		}
+		result += utils.MustAtoi(string(match[1])) * utils.MustAtoi(string(match[2]))
+	}
+	return result
+}
