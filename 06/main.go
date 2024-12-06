7c4eab550a4db7910d62e68e3a3fb5a80a3ad739
spossner <github@possner.de>
2024-12-06 20:30:44 +0100

    scan the route of guards

---
+++ 06/main.go
@@ -0,0 +1,108 @@
+package _0
+
+import (
+	"fmt"
+	"github.com/spossner/aoc2024/internal/config"
+	"github.com/spossner/aoc2024/internal/point"
+	"github.com/spossner/aoc2024/internal/puzzle"
+	"github.com/spossner/aoc2024/internal/rectangle"
+	"github.com/spossner/aoc2024/internal/utils"
+)
+
+const DAY = 6
+
+var DIRECTIONS = map[string]point.Point{
+	"^": point.UP,
+	"v": point.DOWN,
+	"<": point.LEFT,
+	">": point.RIGHT,
+}
+
+func createConfig(dev bool) *config.Config {
+	fmt.Print(utils.GetFileName())
+	return config.NewConfig(DAY, dev, config.WithSplitWords("")) // split lines only
+}
+
+func part1(dev bool) any {
+	return solve(createConfig(dev), false)
+}
+
+func part2(dev bool) any {
+	return solve(createConfig(dev), true)
+}
+
+func findGuard(cells [][]string) (pos point.Point, direction point.Point, ok bool) {
+	for y, row := range cells {
+		for x, cell := range row {
+			if direction, ok := DIRECTIONS[cell]; ok {
+				return point.Point{x, y}, direction, true
+			}
+		}
+	}
+	return point.Point{}, point.Point{}, false
+}
+
+type Guard struct {
+	pos       point.Point
+	direction point.Point
+}
+
+func findPath(pos, direction point.Point, cells [][]string) (utils.Set[Guard], bool) {
+	seen := utils.NewSet(Guard{pos, direction})
+	bounds := rectangle.NewBounds(cells)
+	for {
+		newPos := pos.Translate(direction.X, direction.Y)
+		if !bounds.Contains(newPos) {
+			break
+		}
+		if cells[newPos.Y][newPos.X] == "#" {
+			direction = direction.RotateRight()
+			continue
+		}
+		newGuard := Guard{newPos, direction}
+		if seen.Contains(newGuard) {
+			return utils.Set[Guard]{}, false
+		}
+		seen.Add(newGuard)
+		pos = newPos
+	}
+	return seen, true
+}
+
+func solve(cfg *config.Config, isPart2 bool) any {
+	p := puzzle.NewPuzzle(cfg)
+	result := 0
+	if isPart2 {
+		fmt.Println("PART2")
+
+	}
+
+	pos, direction, ok := findGuard(p.Cells)
+	utils.AssertTrue(ok)
+
+	if isPart2 {
+		for y, row := range p.Cells {
+			for x, cell := range row {
+				if x == pos.X && y == pos.Y {
+					continue
+				}
+				if cell == "#" {
+					continue
+				}
+
+				p.Cells[y][x] = "#"
+				_, ok := findPath(pos, direction, p.Cells)
+				if !ok {
+					result++
+				}
+				p.Cells[y][x] = "."
+			}
+		}
+	} else {
+		seen, _ := findPath(pos, direction, p.Cells)
+		fmt.Println(pos, seen)
+		result = len(seen)
+	}
+
+	return result
+}
