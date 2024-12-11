a8b4cc07fb4c94feca948aaf45b3678353c5579d
spossner <github@possner.de>
2024-12-11 16:48:41 +0100

    calculate number of stones after they are splitted in every step

---
+++ 11/main.go
@@ -0,0 +1,79 @@
+package _0
+
+import (
+	"fmt"
+	"github.com/spossner/aoc2024/internal/config"
+	"github.com/spossner/aoc2024/internal/puzzle"
+	"github.com/spossner/aoc2024/internal/utils"
+	"strconv"
+)
+
+const DAY = 11
+
+func createConfig(dev bool) *config.Config {
+	utils.AssertNotEqual(DAY, -1, "did not configure the DAY")
+	return config.NewConfig(DAY, dev, config.NoLineSplit, config.GetInts)
+}
+
+func part1(dev bool) any {
+	return solve(createConfig(dev), false)
+}
+
+func part2(dev bool) any {
+	return solve(createConfig(dev), true)
+}
+
+func splitStone(v int) []int {
+	if v == 0 {
+		return []int{1}
+	}
+
+	s := strconv.Itoa(v)
+	n := len(s)
+	if n%2 == 0 {
+		v1, _ := strconv.Atoi(s[:n>>1])
+		v2, _ := strconv.Atoi(s[n>>1:])
+		return []int{v1, v2}
+	}
+	return []int{v * 2024}
+}
+
+func countStones(dp map[int]map[int]int, v int, n int) int {
+	//fmt.Println("Count", v, "after", n, "steps...")
+	counts, ok := dp[v] // is v already calculated?
+	if !ok {
+		counts = make(map[int]int)
+		counts[0] = 1
+		dp[v] = counts
+	}
+	sum, ok := counts[n]
+	if ok {
+		return sum
+	}
+
+	for _, subValue := range splitStone(v) {
+		sum += countStones(dp, subValue, n-1)
+	}
+	counts[n] = sum
+	//fmt.Printf("%d[%d] := %d\n", v, n, sum)
+	return sum
+}
+
+func solve(cfg *config.Config, isPart2 bool) any {
+	p := puzzle.NewPuzzle(cfg)
+	result := 0
+	if isPart2 {
+		fmt.Println("PART2")
+	}
+
+	n := 25
+	if isPart2 {
+		n = 75
+	}
+	dp := make(map[int]map[int]int)
+	for _, v := range p.ParsedRows {
+		result += countStones(dp, v, n)
+	}
+
+	return result
+}
