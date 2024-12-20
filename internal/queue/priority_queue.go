4035cdd9977bb19df1983ead2fc4936c1981a141
spossner <github@possner.de>
2024-12-20 13:41:22 +0100

    priority queue which can be used in conjunction with container.heap

---
+++ internal/queue/priority_queue.go
@@ -0,0 +1,45 @@
+package queue
+
+import "github.com/spossner/aoc2024/internal/point"
+
+type Item struct {
+	Pos   point.Point
+	Costs int
+	index int // used by heap
+}
+
+func NewItem(pos point.Point, costs int) *Item {
+	return &Item{Pos: pos, Costs: costs}
+}
+
+type PriorityQueue []*Item
+
+func (pq PriorityQueue) Len() int { return len(pq) }
+func (pq PriorityQueue) Less(i, j int) bool {
+	return pq[i].Costs < pq[j].Costs
+}
+func (pq PriorityQueue) Swap(i, j int) {
+	pq[i], pq[j] = pq[j], pq[i]
+	pq[i].index = i
+	pq[j].index = j
+}
+func (pq PriorityQueue) Empty() bool {
+	return len(pq) == 0
+}
+
+func (pq *PriorityQueue) Push(x any) {
+	n := len(*pq)
+	item := x.(*Item)
+	item.index = n
+	*pq = append(*pq, item)
+}
+
+func (pq *PriorityQueue) Pop() any {
+	old := *pq
+	n := len(old)
+	item := old[n-1]
+	old[n-1] = nil  // don't stop the GC from reclaiming the item eventually
+	item.index = -1 // for safety
+	*pq = old[0 : n-1]
+	return item
+}
