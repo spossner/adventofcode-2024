f8f435cca41979b3fff18389d6967bd61d97e311
spossner <github@possner.de>
2025-01-02 13:46:18 +0100

    refactor to not create all candidates but only count the number of key presses needed... recursively through all robot steps

--- 21/main.go
+++ 21/main.go
@@ -8,8 +8,6 @@ import (
 	"github.com/spossner/aoc2024/internal/puzzle"
 	"github.com/spossner/aoc2024/internal/utils"
 	"math"
-	"os"
-	"strconv"
 	"strings"
 )
 
@@ -42,6 +40,11 @@ var DIRECTIONAL = grid.AsGrid([][]string{
 	{"<", "v", ">"},
 })
 
+type steps struct {
+	sequence string
+	robot    int
+}
+
 func solve(cfg *config.Config, isPart2 bool) any {
 	defer utils.Duration(fmt.Sprintf("DAY %d, PART %d", cfg.Day, utils.If(isPart2, 2, 1)))()
 
@@ -51,98 +54,51 @@ func solve(cfg *config.Config, isPart2 bool) any {
 		fmt.Println("PART2")
 	}
 
+	cache := make(map[steps]int)
 	for _, sequence := range p.Rows {
-		candidates := shortestSequences(findPaths(NUMERIC, sequence, false))
-
-		steps := utils.If(isPart2, 25, 2)
-		for i := range steps {
-			newCandidates := []string{}
-			for j, c := range candidates {
-				fmt.Println(sequence, i, j, len(c))
-				fmt.Print(".")
-				newCandidates = shortestSequences(append(newCandidates, findPaths(DIRECTIONAL, c, true)...))
-			}
-			fmt.Println()
-			candidates = newCandidates
+		v, ok := strings.CutSuffix(sequence, "A")
+		if !ok {
+			panic("illegal sequence (not ending at A): " + sequence)
 		}
+		no := utils.MustAtoi(v)
 
-		fmt.Println(sequence, minLength(candidates))
-
-		m := minLength(candidates)
-		if code, ok := strings.CutSuffix(sequence, "A"); ok {
-			no, _ := strconv.Atoi(code)
-			result += no * m
-			continue
-		}
-		os.Exit(1)
+		robots := utils.If(isPart2, 25, 2)
+		result += no * calculateKeyPresses(NUMERIC, sequence, robots, cache)
 	}
 
 	return result
 }
 
-func shortestSequences(candidates []string) []string {
-	m := minLength(candidates)
-	return utils.MustFilter(candidates, func(path string) (bool, error) {
-		return len(path) == m, nil
-	})
-}
+func calculateKeyPresses(g grid.Grid, sequence string, robot int, cache map[steps]int) int {
+	key := steps{sequence, robot}
+	if v, ok := cache[key]; ok {
+		return v
+	}
 
-func minLength(candidates []string) int {
-	return utils.Reduce[int, int](
-		utils.MustMap[string, int](
-			candidates,
-			func(path string) (int, error) {
-				return len(path), nil
-			},
-		),
-		func(acc int, item int) int {
-			return utils.If(item < acc, item, acc)
-		},
-		math.MaxInt,
-	)
-}
-
-var cache = make(map[string][]string)
-
-func findPaths(g grid.Grid, sequence string, withCache bool) []string {
-	var candidates []string
 	var s, e point.Point
+	length := 0
 	e, _ = g.FindMarker("A")
 	for i := 0; i < len(sequence); i++ {
 		s = e
 		e, _ = g.FindMarker(string(sequence[i]))
-		iconStart := g.Get(s.X, s.Y)
-		iconEnd := g.Get(e.X, e.Y)
 
-		var paths []string
-		if withCache {
-			if v, ok := cache[iconStart+iconEnd]; ok {
-				candidates = product(candidates, v)
-				continue
-			}
-		}
-
-		solutions := g.BfsAll(s, e, grid.WithDirections())
-		paths = utils.MustMap[grid.Path, string](solutions, func(path grid.Path) (string, error) {
+		moves := g.BfsAll(s, e, grid.WithDirections())
+		paths := utils.MustMap[grid.Path, string](moves, func(path grid.Path) (string, error) {
 			return path.String() + "A", nil
 		})
-		if withCache {
-			cache[iconStart+iconEnd] = paths
-		}
-		candidates = product(candidates, paths)
-	}
-	return candidates
-}
 
-func product(candidates []string, nextCandidates []string) []string {
-	combinedCandidates := make([]string, 0, len(candidates)*len(nextCandidates))
-	if candidates == nil {
-		return nextCandidates
-	}
-	for _, c := range candidates {
-		for _, d := range nextCandidates {
-			combinedCandidates = append(combinedCandidates, c+d)
+		if robot == 0 {
+			length += len(utils.PickFirst(paths))
+		} else {
+			best := math.MaxInt
+			for _, move := range paths {
+				// check key presses of all paths in subsequent directional keypads... and choose the one with min length
+				best, _ = utils.MinMax(best, calculateKeyPresses(DIRECTIONAL, move, robot-1, cache))
+			}
+			length += best
 		}
 	}
-	return combinedCandidates
+
+	cache[key] = length
+	return length
 }
